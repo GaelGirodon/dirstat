2986c92910c33276391d25eb44f4f0f8f1dde3a2
Gael Girodon <contact@gaelgirodon.fr>
2026-02-11 21:51:41 +0100

    Update dependencies

--- report/vite.config.js
+++ report/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import preact from "@preact/preset-vite";
 import autoprefixer from "autoprefixer";
 import nested from "postcss-nested";
-import { purgeCSSPlugin } from "@fullhuman/postcss-purgecss";
+import purgeCSSPlugin from "@fullhuman/postcss-purgecss";
 
 import { publishPlugin, stubPlugin } from "./vite.plugins";
 
